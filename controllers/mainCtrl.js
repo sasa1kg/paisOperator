295c162b5420ea5689a2d00071745bdaf6196f6f
unknown <rsasa@RTRKN039.domain.local>
2015-09-23 11:30:08 +0200

    Commit with image upload and backbone set.

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -5,6 +5,22 @@ angular.module('paisOperator').controller("mainCtrl", ["$scope", "$http", "$filt
 
     scope.clientId = 22;
 
+    scope.selected = {
+        "type" : -1,
+        "id" : -1
+    };
+
+    scope.select = function (type, orderId) {
+        scope.selected.type = type;
+        scope.selected.id = orderId;
+        if (type == 1) {
+            for (var i = scope.sensors.length - 1; i >= 0; i--) {
+                if (scope.sensors[i].id == orderId) {
+                    setMarkers(scope.map, scope.sensors[i]);
+                }
+            };
+        }
+    }
 
     scope.accounts = [
             {
@@ -21,12 +37,47 @@ angular.module('paisOperator').controller("mainCtrl", ["$scope", "$http", "$filt
             }
     ];
 
-    scope.selectedAccount = scope.accounts[0];
+    scope.sensors = [
+            {
+                "type" : "Temperatura vazduha",
+                "id" : "1",
+                "latitude" : 44,
+                "longitude" :  20.461414
+            },
+            {
+                "type" : "Temperatura vazduha",
+                "id" : "2",
+                "latitude" : 43.954545,
+                "longitude" :  20.161414
+            },
+            {
+                "type" : "Vlaznost vazduha",
+                "id" : "3",
+                "latitude" : 44.1678,
+                "longitude" :  20.23
+            },
+            {
+                "type" : "Temperatura vazduha",
+                "id" : "4",
+                "latitude" : 43.9887,
+                "longitude" :  20.02975
+            },
+            {
+                "type" : "Vlaznost vazduha",
+                "id" : "5",
+                "latitude" : 43.78435,
+                "longitude" :  19.65342
+            }
+    ];
+
+
     
 
-    scope.isActive = function (order) {
-        if (scope.selectedAccount != null) {
-           return scope.selectedAccount.order_id == order;
+    scope.isActive = function (type, order) {
+        if (scope.selected != null) {
+            if (type == scope.selected.type && scope.selected.id == order) {
+                return true;
+            }
         } else {
            return false;
         }
@@ -57,6 +108,58 @@ angular.module('paisOperator').controller("mainCtrl", ["$scope", "$http", "$filt
             }   
         });
     }
+
+    var myLatlng = new google.maps.LatLng(44, 20.461414);
+
+    $(document).ready(function () {
+
+        var mapCanvasId = 'map-canvas-results',
+        myOptions = {
+            center: myLatlng,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            zoom: 8,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.LARGE,
+                position: google.maps.ControlPosition.LEFT_CENTER
+            }
+        }
+        scope.map = new google.maps.Map(document.getElementById(mapCanvasId), myOptions);
+    });
+
+
+    function setMarkers(map, sensor) {
+        if (scope.sensorOnMap != undefined && scope.sensorOnMap != null) {
+            scope.sensorOnMap.setMap(null);
+        }
+            var bounds = new google.maps.LatLngBounds();
+
+
+            var coords = new google.maps.LatLng(sensor.latitude, sensor.longitude);
+            var contentString = "Sensor ID " + sensor.id;
+            var infowindow = new google.maps.InfoWindow({content: contentString});
+            
+
+
+            var marker = new google.maps.Marker({
+                position: coords,
+                map: map,
+                icon: "img/waterfilter.png"
+            });
+            google.maps.event.addListener(marker, 'click', 
+                function (infowindow, marker) {
+                    return function () {
+                        infowindow.open(map, marker);
+                    };
+                }(infowindow, marker)
+            );
+            scope.sensorOnMap = marker;
+            bounds.extend(coords);
+            scope.map.fitBounds(bounds);
+        }
+    
+
+    scope.sensorsOnMap;
 
 
 }]);
